9d51ff49eba6dfc05436a14061f472e6aae3b453
vfscalfani <vincent.scalfani@gmail.com>
2019-06-11 21:32:47 -0500

    Updated PubChem_Similarity

--- m_scripts/PubChem_Similarity.m
+++ m_scripts/PubChem_Similarity.m
@@ -5,9 +5,13 @@
 % Vincent F. Scalfani, Serena C. Ralph, and Jason E. Bara 
 % The University of Alabama
 % Version: 1.0, created with MATLAB R2018a
+%% Define the PubChem API base URL
 
-% Define PubChem API
+% PubChem API
 api = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/';
+
+% set MATLAB web options to a 30 second timeout
+options = weboptions('Timeout', 30);
 
 % Retrieve and display PNG Image of Hepoxilin A4; CID = 6438949
 CID_SS_query = '6438949';
@@ -15,8 +19,8 @@ CID_SS_query = '6438949';
 CID_url = [api 'cid/' CID_SS_query '/PNG'];
 [CID_img,map] = imread(CID_url);
 imshow(CID_img,map)
-% set MATLAB web options to a 30 second timeout
-options = weboptions('Timeout', 30);
+%% 
+% _Replace the above CID value (CID_SS_query) with a different CID to customize._
 %% Retrieve InChI and SMILES
 
 % Retrieve InChI
@@ -35,7 +39,15 @@ api = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/';
 SS_url = [api 'fastsimilarity_2d/cid/' CID_SS_query '/cids/JSON?Threshold=95'];
 SS_CIDs = webread(SS_url,options);
 SS_CIDs = num2cell(SS_CIDs.IdentifierList.CID)
-% set a CID limit to 100 max (can be higher, but useful for initial testing)
+%% 
+% _In the above SS_url value, you can adjust to the desired Tanimoto threshold 
+% (i.e., 97, 90, etc.)_
+
+% set a CID limit to 100 max
+%% 
+% _The CID limit of 100 was added as an initial testing safety for time 
+% consideration. This limit can be increased._
+
 number_SS_CIDs = length(SS_CIDs)
 if number_SS_CIDs > 100
         
@@ -49,6 +61,7 @@ end
 % Retrieve the following data from CID hit results:
 % InChI, Isomeric SMILES, MW, HBond Donor Count, HBond Acceptor Count, XLogP
 
+% setup a for loop that processes each CID one-by-one
 for r = 1:length(SS_CIDs)
     CID = SS_CIDs{r};
     
@@ -60,7 +73,17 @@ for r = 1:length(SS_CIDs)
     CID_HBond_D_url = [api 'cid/' num2str(CID) '/property/HBondDonorCount/TXT'];
     CID_HBond_A_url = [api 'cid/' num2str(CID) '/property/HBondAcceptorCount/TXT'];
     CID_XLogP_url = [api 'cid/' num2str(CID) '/property/XLogP/TXT'];
-        
+    
+    
+%% 
+% _Additional property data can be collected by defining new api calls, 
+% for example, if you want TPSA data:_
+
+% CID_TPSA_url = [api 'cid/' num2str(CID) '/property/TPSA/TXT'];
+%% 
+% 
+
+       
     % retrieve identifer and property data
     try
         CID_InChI = webread(CID_InChI_url,options);      
@@ -111,13 +134,21 @@ for r = 1:length(SS_CIDs)
         n = 0.5;
         pause(n)
         
-      % add property data to SS_CIDs data array 
+      % add property data to SS_CIDs data array
+      
+      % column numbers indicate where the data will be stored.
+      % For example, the MW will be placed in column 4. r increases
+      % by 1 on each iteration, so the first CID_MW value gets stored in
+      % {1,4}, the second in {2,4}, the third in {3,4}, etc.
         SS_CIDs{r,2} = CID_InChI;
         SS_CIDs{r,3} = CID_IsoSMI;
         SS_CIDs{r,4} = CID_MW;
         SS_CIDs{r,5} = CID_HBond_D;
         SS_CIDs{r,6} = CID_HBond_A;
         SS_CIDs{r,7} = CID_XLogP;
+        
+        % to add more data, simply index into the next column
+        % SS_CIDs{r,8} = CID_TPSA;
                                        
 end
 
